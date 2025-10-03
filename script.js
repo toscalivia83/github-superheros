27edd79e114a295a96669984f5c428f621ad023d
Damiussy <dracuglar@gmail.com>
2025-10-03 09:16:39 +0200

    Titre et Css

--- script.js
+++ script.js
@@ -7,24 +7,10 @@
 // 4. Faites des commits réguliers !
 // ========================================
 
-const heroId = 620; // 👈 CHANGEZ CET ID !
+const heroId = 346; // 👈 CHANGEZ CET ID ! (346 = Iron Man)
 
 // Liste des IDs disponibles :
 // Spider-Man: 620, Batman: 70, Iron Man: 346, Superman: 644
 // Wonder Woman: 720, Hulk: 332, Thor: 659, Flash: 263
-const myToken = ""; // Ajoutez le token donné dans le cours
-const apiUrl = `https://superheroapi.com/api.php/${myToken}/${heroId}`;
-
-// Récupérer les données du héros avec fetch()
-// et les afficher grâce à Javascript dans le HTML de cette manière :
-
-{{/* <h2>${data.name}</h2>
-<img src="${heroImageUrl}" alt="${data.name}" height="200">
-<p><strong>Nom complet :</strong> ${data.biography['full-name']}</p>
-<p><strong>Éditeur :</strong> ${data.biography.publisher}</p>
-<p><strong>Intelligence :</strong> ${data.powerstats.intelligence}/100</p>
-<p><strong>Force :</strong> ${data.powerstats.strength}/100</p> */}}
-
-// utilisez heroImageUrl = "https://corsproxy.io/?" + encodeURIComponent(data.image.url);
-
-// N'oubliez pas de gérer les erreurs (avec .catch())
+const myToken = "4905856019427443"; // ⚠️ Remplacez par le token donné dans le cours
+const apiUrl = `https://www.superheroapi.com/api.php/${myToken}/${heroId}`;
