65a5150a689eef023d71987d87cbc3e5cd052c06
althahir <romain.sebastiani@gmail.com>
2025-10-03 12:13:04 +0200

    Ajout image Thor

--- Thor/thor.js
+++ Thor/thor.js
@@ -6,7 +6,7 @@
 // 3. Ajoutez du CSS dans index.html si vous voulez
 // 4. Faites des commits réguliers !
 // ========================================
-
+;
 const heroId = 659; // 👈 CHANGEZ CET ID !
 
 // Liste des IDs disponibles :
@@ -16,21 +16,33 @@ const myToken = `6570e44801f81594f8a913d3e21be5ab`;
 const apiUrl = `https://superheroapi.com/api.php/${myToken}/${heroId}`;
 
 fetch(apiUrl)
-.then(response => response.json()) // Convertit la réponse en JSON
+.then(response => response.json())
   .then(data => {
-    const heroImageUrl = "https://corsproxy.io/?" + encodeURIComponent(data.image.url); // ou le champ correspondant dans ton JSON
+    console.log(data)
+ 
+      const titre = document.getElementById("titre")
+      titre.innerHTML = data.name
 
-    // Construire le HTML
-    const heroHtml = `
-      <h2>${data.name}</h2>
-      <img src="${heroImageUrl}" alt="${data.name}" height="200">
-      <p><strong>Nom complet :</strong> ${data.biography['full-name']}</p>
-      <p><strong>Éditeur :</strong> ${data.biography.publisher}</p>
-      <p><strong>Intelligence :</strong> ${data.powerstats.intelligence}/100</p>
-      <p><strong>Force :</strong> ${data.powerstats.strength}/100</p>
-    `;
-    document.getElementById("hero-info").innerHTML = heroHtml;
-  }) // ou le champ correspondant dans ton JSON
+      const heroImageUrl = "https://corsproxy.io/?" + encodeURIComponent(data.image.url);
+      document.getElementById("image").innerHTML= `<img src="${heroImageUrl}" alt="Thor">`;
+
+      const full_name = document.getElementById("full_name")
+      full_name.innerHTML = data.biography['full-name']
+      
+      const editeur=document.getElementById("editeur")
+      editeur.innerHTML=data.biography.publisher
+
+      const intelligence=document.getElementById("intelligence")
+      intelligence.innerHTML=data.powerstats.intelligence
+
+      const force=document.getElementById("force")
+      force.innerHTML=data.powerstats.strength
+
+    ;
+
+    // Injection dans le DOM
+    // document.getElementById("image").innerHTML = heroHtml;
+  })
  // Affiche les données dans la console
 .catch(error => console.error('Erreur :', error)); // Gestion d'erreur
 
