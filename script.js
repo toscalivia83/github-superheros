8abb61cdc9e8278fb0e069fdf68e0cea3b81ad01
Jadussy <vuotto.jade@gmail.com>
2025-10-03 10:00:01 +0200

    Premier Commit

--- script.js
+++ script.js
@@ -7,24 +7,28 @@
 // 4. Faites des commits réguliers !
 // ========================================
 
-const heroId = 620; // 👈 CHANGEZ CET ID !
+const heroId = 70;
 
-// Liste des IDs disponibles :
-// Spider-Man: 620, Batman: 70, Iron Man: 346, Superman: 644
-// Wonder Woman: 720, Hulk: 332, Thor: 659, Flash: 263
-const myToken = ""; // Ajoutez le token donné dans le cours
+const myToken = "6570e44801f81594f8a913d3e21be5ab"; 
 const apiUrl = `https://superheroapi.com/api.php/${myToken}/${heroId}`;
 
-// Récupérer les données du héros avec fetch()
-// et les afficher grâce à Javascript dans le HTML de cette manière :
+const container = document.getElementById("hero-info");
 
-{{/* <h2>${data.name}</h2>
-<img src="${heroImageUrl}" alt="${data.name}" height="200">
-<p><strong>Nom complet :</strong> ${data.biography['full-name']}</p>
-<p><strong>Éditeur :</strong> ${data.biography.publisher}</p>
-<p><strong>Intelligence :</strong> ${data.powerstats.intelligence}/100</p>
-<p><strong>Force :</strong> ${data.powerstats.strength}/100</p> */}}
+fetch(apiUrl)
+    .then( ( reponse ) => reponse.json () )
+    .then ( ( data ) => {
+        const heroImageUrl = "https://corsproxy.io/?" + encodeURIComponent(data.image.url);
 
-// utilisez heroImageUrl = "https://corsproxy.io/?" + encodeURIComponent(data.image.url);
+        container.innerHTML = `
+        <h2>${data.name}</h2>
+        <img src="${heroImageUrl}" alt="${data.name}" height="200">
+        <p><strong>Nom complet :</strong> ${data.biography['full-name']}</p>
+        <p><strong>Éditeur :</strong> ${data.biography.publisher}</p>
+        <p><strong>Intelligence :</strong> ${data.powerstats.intelligence}/100</p>
+        <p><strong>Force :</strong> ${data.powerstats.strength}/100</p> `;
+    })
+    .catch( ( error ) => console.log("Erreur : ", error));
+
 
 // N'oubliez pas de gérer les erreurs (avec .catch())
+// J'ai ca : GET http://127.0.0.1:5500/$%7BheroImageUrl%7D 404 (Not Found)
