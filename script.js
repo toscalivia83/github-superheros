40cea2ec996a461379508cf4e4b782e9b7b36c0f
Jadussy <vuotto.jade@gmail.com>
2025-10-03 10:21:24 +0200

    Ajout de plus de description du super-héros

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const apiUrl = `https://superheroapi.com/api.php/${myToken}/${heroId}`;
 
 const container = document.getElementById("hero-info");
 
+// https://superheroapi.com/api.php/6570e44801f81594f8a913d3e21be5ab/70
 fetch(apiUrl)
     .then( ( reponse ) => reponse.json () )
     .then ( ( data ) => {
@@ -25,7 +26,14 @@ fetch(apiUrl)
         <p><strong>Nom complet :</strong> ${data.biography['full-name']}</p>
         <p><strong>Éditeur :</strong> ${data.biography.publisher}</p>
         <p><strong>Intelligence :</strong> ${data.powerstats.intelligence}/100</p>
-        <p><strong>Force :</strong> ${data.powerstats.strength}/100</p> `;
+
+        <p><strong>Rapidité :</strong> ${data.powerstats.speed}/100</p> 
+
+        <p><strong>Lieu de naissance :</strong> ${data.biography['place-of-birth']}</p> 
+        
+        <p><strong>Taille :</strong> ${data.appearance.height[1]}</p> 
+        
+        `;
     })
     .catch( ( error ) => console.log("Erreur : ", error));
 
